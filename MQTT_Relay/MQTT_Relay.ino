12b17b9658fb93d10ef6e6dc068c83414a26171a
Chia-Chun Wu <ccwu0918@gmail.com>
2023-12-14 12:51:49 +0800

    Add files via upload

--- MQTT_Relay/MQTT_Relay.ino
+++ MQTT_Relay/MQTT_Relay.ino
@@ -1,22 +1,15 @@
-/*
- * Generated using BlocklyDuino:
- *
- * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
- *
- * Date: Sun, 07 Nov 2021 18:46:40 GMT
- */
-/*  部份程式由吉哥積木產生  */
-/*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
+//Generated Date: Thu, 14 Dec 2023 04:50:44 GMT
+
 #include <LWiFi.h>
 #include <PubSubClient.h>
-
-char _lwifi_ssid[] = "HUAWEI_E5576_A5C5";
-char _lwifi_pass[] = "0972866968";
-#define MQTT_SERVER_IP "broker.emqx.io"
+#define MQTT_SERVER_IP "broker.mqttgo.io"
 #define MQTT_SERVER_PORT 1883
 #define MQTT_ID ""
 #define MQTT_USERNAME ""
 #define MQTT_PASSWORD ""
+
+char _lwifi_ssid[] = "iPhone12Pro";
+char _lwifi_pass[] = "0972866968";
 String receivedTopic="";
 String receivedMsg="";
 bool waitForE=true;
@@ -40,7 +33,7 @@ void mqttCallback(char* topic, byte* payload, unsigned int length){
   receivedMsg="";
   for (unsigned int myIndex = 0; myIndex < length; myIndex++)
   {
-      receivedMsg += (char)payload[myIndex];
+    receivedMsg += (char)payload[myIndex];
   }
   receivedMsg.trim();
   if (receivedTopic == "ccwu0918/relay") {
@@ -59,7 +52,6 @@ void setup()
   myClient.subscribe(String("ccwu0918/relay").c_str());
   pinMode(5, OUTPUT);
 }
-
 
 void loop()
 {
