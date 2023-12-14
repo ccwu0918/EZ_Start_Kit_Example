69f30429acb6a85878dcd9d7491ce82d8fcba17c
Chia-Chun Wu <ccwu0918@gmail.com>
2023-12-14 12:47:15 +0800

    Add files via upload

--- MQTT_DHT11_Relay_PHR_VR_LED/MQTT_DHT11_Relay_PHR_VR_LED.ino
+++ MQTT_DHT11_Relay_PHR_VR_LED/MQTT_DHT11_Relay_PHR_VR_LED.ino
@@ -1,16 +1,14 @@
-/*
- * Generated using BlocklyDuino:
- *
- * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
- *
- * Date: Sun, 05 Dec 2021 18:33:24 GMT
- */
-/*  部份程式由吉哥積木產生  */
-/*  https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697  */
+//Generated Date: Thu, 14 Dec 2023 04:46:13 GMT
+
 #include "Wire.h"
 #include "U8g2lib.h"
 #include <LWiFi.h>
 #include <PubSubClient.h>
+#define MQTT_SERVER_IP "broker.mqttgo.io"
+#define MQTT_SERVER_PORT 1883
+#define MQTT_ID ""
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
 #include <DHT.h>
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
@@ -18,11 +16,6 @@ int clock_center_x=64;
 int clock_center_y=32;
 char _lwifi_ssid[] = "iPhone12Pro";
 char _lwifi_pass[] = "0972866968";
-#define MQTT_SERVER_IP "broker.emqx.io"
-#define MQTT_SERVER_PORT 1883
-#define MQTT_ID ""
-#define MQTT_USERNAME ""
-#define MQTT_PASSWORD ""
 String receivedTopic="";
 String receivedMsg="";
 bool waitForE=true;
@@ -31,6 +24,11 @@ bool pubCtrl=false;
 
 WiFiClient mqttClient;
 PubSubClient myClient(mqttClient);
+
+DHT dht11_p10(10, DHT11);
+byte r_pin=13;
+byte y_pin=12;
+byte g_pin=11;
 
 void connectMQTT(){
   while (!myClient.connected()){
@@ -41,17 +39,12 @@ void connectMQTT(){
   }
 }
 
-DHT dht11_p10(10, DHT11);
-byte r_pin=13;
-byte y_pin=12;
-byte g_pin=11;
-
 void mqttCallback(char* topic, byte* payload, unsigned int length){
   receivedTopic=String(topic);
   receivedMsg="";
   for (unsigned int myIndex = 0; myIndex < length; myIndex++)
   {
-      receivedMsg += (char)payload[myIndex];
+    receivedMsg += (char)payload[myIndex];
   }
   receivedMsg.trim();
   if (receivedTopic == "ccwu0918/relay") {
@@ -97,7 +90,6 @@ void setup()
   pinMode(y_pin, OUTPUT);
   pinMode(g_pin, OUTPUT);
 }
-
 
 void loop()
 {
